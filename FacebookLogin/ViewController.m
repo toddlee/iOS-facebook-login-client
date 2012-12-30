9261b06d8cbf7752a301910fb52a59226853d181
Todd Lee <taewon@gmail.com>
2012-12-29 22:55:12 -0800

    Add facebook login view to the app view

--- FacebookLogin/ViewController.m
+++ FacebookLogin/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
 
 @end
@@ -17,7 +16,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
 	// Do any additional setup after loading the view, typically from a nib.
+    FBLoginView *loginview = [[FBLoginView alloc] init];
+    
+    loginview.center = self.view.center;
+    loginview.delegate = self;
+    
+    [self.view addSubview:loginview];
+    
+    [loginview sizeToFit];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +34,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
+{
+    NSLog(@"Logged in user: %@", user);
+    NSLog(@"Access token: %@", [[FBSession activeSession] accessToken]);
+}
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView
+{
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
+{
+}
 @end
